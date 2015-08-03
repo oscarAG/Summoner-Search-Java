c7a79f8857e041d937c11e285bf71afc5e2d1cf4
Oscar <Oscar@oscar_desktop>
2015-08-03 16:36:39 -0500

    Version 1.1.2. Fixed the issue with the combo box on the main page leaving a grey shadow. Made some code a bit more readable.

--- src/lol/search/GUIFrame.java
+++ src/lol/search/GUIFrame.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
  * @author Oscar
  */
 public class GUIFrame {
-    private final String version = "1.1.1";
+    private final String version = "1.1.2";
     private final String applicationName = "League of Legends Player Search " + version; //title bar name of the application
     private final JFrame frame; //main frame of the application
     
