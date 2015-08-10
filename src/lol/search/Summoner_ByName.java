49b196a5f5158d44cc10804112c8e8e65ce38904
Oscar <Oscar@DESKTOP-O8PB9GV>
2015-08-10 14:58:12 -0500

    Miniscule code changes.

--- src/lol/search/Summoner_ByName.java
+++ src/lol/search/Summoner_ByName.java
@@ -7,14 +7,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
